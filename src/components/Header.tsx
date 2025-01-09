392a9b9ced929969bf73657f70eb379eb879131c
Abner Kaizer <mail@abnerkaizer.com>
2025-01-09 17:43:08 -0300

    feat: added search for tickets and customers

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,43 @@
-import { HomeIcon, File, UsersRound, LogOut } from "lucide-react"
-import Link from "next/link"
-import {NavButton} from '@/components/NavButton'
-import { ModeToggle } from "./ModeToggle"
-import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components"
-import { Button } from "./ui/button"
+import { HomeIcon, File, UsersRound, LogOut } from 'lucide-react';
+import Link from 'next/link';
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs/components';
+
+import { Button } from '@/components/ui/button';
+import { NavButton } from '@/components/NavButton';
+import { ModeToggle } from '@/components/ModeToggle';
+import { NavButtonMenu } from './NavButtonMenu';
 
 export function Header() {
-    return(
-        <header className="animate-slide bg-backgroud h-12 p-2 border-b
-        sticky top-0 z-20">
-            <div className="flex h-8 item-center justify-between w-full">
+    return (
+        <header className="animate-slide bg-background h-12 p-2 border-b sticky top-0 z-20">
+
+            <div className="flex h-8 items-center justify-between w-full">
+
                 <div className="flex items-center gap-2">
-                    <NavButton href="/home" label="Home" icon={HomeIcon}/>
+                    <NavButton href="/home" label="Home" icon={HomeIcon} />
+
                     <Link href="/home" className="flex justify-center items-center gap-2 ml-0" title="Home">
                         <h1 className="hidden sm:block text-xl font-bold m-0 mt-1">
                             Computer Repair Shop
                         </h1>
                     </Link>
                 </div>
-                <div className="flex items-center">
-                
-                    <NavButton href="/tickets" label="Tickets" icon={File}/>
 
-                    <NavButton href="/customers" label="Customers" icon={UsersRound}/>
-                    <ModeToggle/>
+                <div className="flex items-center">
+
+                    <NavButton href="/tickets" label="Tickets" icon={File} />
+
+                    <NavButtonMenu
+                        icon={UsersRound}
+                        label="Customers Menu"
+                        choices={[
+                            { title: "Search Customers", href: "/customers" },
+                            { title: "New Customer", href: "/customers/form" }
+                        ]}
+                    />
+
+                    <ModeToggle />
+
                     <Button
                         variant="ghost"
                         size="icon"
@@ -33,11 +47,14 @@ export function Header() {
                         asChild
                     >
                         <LogoutLink>
-                            <LogOut/>
+                            <LogOut />
                         </LogoutLink>
                     </Button>
+
                 </div>
+
             </div>
+
         </header>
     )
 }
